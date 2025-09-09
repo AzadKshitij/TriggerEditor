3e60c0165344c0baef39d4420989147726394202
Ashvinchandra Sanodariya, Kshitij <kashvinchandrasan+deloitte@deloitte.com>
2025-09-09 17:44:30 +0530

    updated run.bat for

--- src/run.bat
+++ src/run.bat
@@ -1,1 +1,17 @@
+@REM echo off
+echo %1
+IF %1 == self GOTO SELF
+IF %1 == compn GOTO COMPN
+GOTO End1
+
+:SELF
 python -m trigger_designer.main "C:\Projects\TriggerEditor\savedfiles\0. All Node- Test.tds"
+exit
+
+:COMPN
+python -m trigger_designer.main "C:\Users\KASHVINCHANDRASAN\Desktop\Personal\Github\TriggerEditor\savedfiles\0. All Node- Test.tds"
+exit
+
+:End1
+python -m trigger_designer.main "C:\Projects\TriggerEditor\savedfiles\0. All Node- Test.tds"
+exit
